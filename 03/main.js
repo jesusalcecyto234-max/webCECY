ca2d0487b7f659a3e5dfede47a8d1f8938e95519
Jesus <jesusalcecyto234@gmail.com>
2026-05-11 08:58:32 -0600

    segundo commit

---
+++ 03/main.js
@@ -0,0 +1,37 @@
+var alto=document.getElementById("alto");
+var ancho=document.getElementById("ancho");
+var color=document.getElementById("color");
+var figura=document.getElementById("figura");
+var radius=document.getElementById("radius")
+var anchoborde=document.getElementById("anchoborde");
+var colorborde=document.getElementById("colorborde");
+var tipoborde=document.getElementById("tipoborde");
+
+
+alto.oninput=()=>{
+    figura.style.height=alto.value+"px";
+    document.getElementById("valto").innerHTML=+alto.value;
+
+}
+ancho.oninput=()=>{
+    figura.style.width=ancho.value+"px";
+    document.getElementById("vancho").innerHTML=+ancho.value;
+}
+color.onchange=()=>{
+figura.style.backgroundColor=color.value;
+}
+radius.oninput=()=>{
+    figura.style.borderRadius=radius.value+"%";
+    document.getElementById("vradio").innerHTML=radius.value;
+}
+anchoborde.oninput=()=>{
+    figura.style.borderWidth=anchoborde.value+"px";
+    document.getElementById("vanchoborde").innerHTML=+anchoborde.value;
+
+}
+colorborde.onchange=()=>{
+    figura.style.borderColor=colorborde.value;
+}
+tipoborde.onchange=()=>{
+    figura.style.borderStyle=tipoborde.value;
+}
