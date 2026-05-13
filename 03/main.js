c132db7e4912fdbb2eece984c910eb18f776c1d2
Jesus <jesusalcecyto234@gmail.com>
2026-05-13 09:42:50 -0600

    final

--- 03/main.js
+++ 03/main.js
@@ -6,6 +6,25 @@ var radius=document.getElementById("radius")
 var anchoborde=document.getElementById("anchoborde");
 var colorborde=document.getElementById("colorborde");
 var tipoborde=document.getElementById("tipoborde");
+var hsombra=document.getElementById("hsombra");
+var vsombra=document.getElementById("vsombra");
+var anchosombra=document.getElementById("anchosombra");
+var colorsombra=document.getElementById("colorsombra");
+
+function actualizarSombra(){
+
+    figura.style.boxShadow=
+    hsombra.value+"px "+
+    vsombra.value+"px "+
+    anchosombra.value+"px "+
+    colorsombra.value;
+
+    document.getElementById("vhsombra").innerHTML=hsombra.value;
+
+    document.getElementById("vvsombra").innerHTML=vsombra.value;
+
+    document.getElementById("vanchosombra").innerHTML=anchosombra.value;
+}
 
 
 alto.oninput=()=>{
@@ -34,4 +53,19 @@ colorborde.onchange=()=>{
 }
 tipoborde.onchange=()=>{
     figura.style.borderStyle=tipoborde.value;
+}
+hsombra.oninput=()=>{
+    actualizarSombra();
+}
+
+vsombra.oninput=()=>{
+    actualizarSombra();
+}
+
+anchosombra.oninput=()=>{
+    actualizarSombra();
+}
+
+colorsombra.onchange=()=>{
+    actualizarSombra();
 }
